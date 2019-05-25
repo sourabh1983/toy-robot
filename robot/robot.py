8e9820473c3d0b0c2aa6796fabe06ce077404b3d
sourabh <acumen.sourabh@gmail.com>
2019-05-26 01:08:34 +1000

    created robot

--- robot/robot.py
+++ robot/robot.py
@@ -2,6 +2,15 @@ from .position import Position
 from .table import TABLE
 
 
+def is_robot_placed(func):
+    def placed(self):
+        if self.position is None:
+            print('Robot is not placed')
+            return False
+        func(self)
+    return placed
+
+
 # My Toy robot needs a table and initial position
 class ToyRobot:
 
@@ -16,14 +25,53 @@ class ToyRobot:
     Destructive move is when move command leads to negative
     coordinates or exceeding table dimensions
     '''
+    @is_robot_placed
     def move(self):
-        pass
+        if self.position.facing == 'NORTH' and self.position.y < TABLE.WIDTH:
+            self.position.y = self.position.y + 1
+        elif self.position.facing == 'SOUTH' and self.position.y != 0:
+            self.position.y = self.position.y - 1
+        elif self.position.facing == 'EAST' and self.position.x < TABLE.LENGTH:
+            self.position.x = self.position.x + 1
+        elif self.position.facing == 'WEST' and self.position.x != 0:
+            self.position.x = self.position.x - 1
 
+    @is_robot_placed
     def left(self):
-        pass
+        self.position.facing = {
+            'NORTH': 'WEST',
+            'WEST': 'SOUTH',
+            'SOUTH': 'EAST',
+            'EAST': 'NORTH'
+        }.get(self.position.facing)
 
+    @is_robot_placed
     def right(self):
-        pass
+        self.position.facing = {
+            'NORTH': 'EAST',
+            'EAST': 'SOUTH',
+            'SOUTH': 'WEST',
+            'WEST': 'NORTH'
+        }.get(self.position.facing)
 
     def report(self):
-        pass
+        print(
+            f'Output: {self.position.x}, \
+                      {self.position.y}, \
+                      {self.position.facing}'
+        )
+        return (self.position.x, self.position.y, self.position.facing)
+
+    def action_on_robot(self, action):
+        action = action.split()
+        if action[0] == 'PLACE':
+            position = tuple(action[1].split(','))
+            self.place(position)
+        elif action[0] == 'MOVE':
+            self.move()
+        elif action[0] == 'LEFT':
+            self.left()
+        elif action[0] == 'RIGHT':
+            self.right()
+        elif action[0] == 'REPORT':
+            self.report()
--- tests/test_robot.py
+++ tests/test_robot.py
@@ -4,24 +4,96 @@ from robot.robot import ToyRobot
 
 
 def test_place():
-    pass
+    bot = ToyRobot()
+    position = (0, 0, 'NORTH')
+    bot.place(position)
+    assert bot.position.x == 0
+    assert bot.position.y == 0
+    assert bot.position.facing == 'NORTH'
+    assert (0, 0, 'NORTH') == bot.report()
 
 
 def test_move():
-    pass
+    bot = ToyRobot()
+    position = (0, 0, 'NORTH')
+    bot.place(position)
+    bot.move()
+    assert bot.position.x == 0
+    assert bot.position.y == 1
+    assert bot.position.facing == 'NORTH'
+    assert (0, 1, 'NORTH') == bot.report()
 
 
 def test_left():
-    pass
+
+    bot = ToyRobot()
+    position = (0, 0, 'NORTH')
+    bot.place(position)
+    bot.left()
+    assert bot.position.x == 0
+    assert bot.position.y == 0
+    assert bot.position.facing == 'WEST'
+    assert (0, 0, 'WEST') == bot.report()
 
 
 def test_right():
-    pass
+
+    bot = ToyRobot()
+    position = (0, 0, 'NORTH')
+    bot.place(position)
+    bot.right()
+    assert bot.position.x == 0
+    assert bot.position.y == 0
+    assert bot.position.facing == 'EAST'
+    assert (0, 0, 'EAST') == bot.report()
 
 
 def test_action_on_robot():
-    pass
+
+    bot = ToyRobot()
+    position = (0, 0, 'NORTH')
+    bot.place(position)
+    bot.action_on_robot('PLACE 0,0,NORTH')
+    assert bot.position.x == 0
+    assert bot.position.y == 0
+    assert bot.position.facing == 'NORTH'
+    assert (0, 0, 'NORTH') == bot.report()
+    bot.action_on_robot('MOVE')
+    assert bot.position.x == 0
+    assert bot.position.y == 1
+    assert bot.position.facing == 'NORTH'
+    assert (0, 1, 'NORTH') == bot.report()
+    bot.action_on_robot('LEFT')
+    assert bot.position.x == 0
+    assert bot.position.y == 1
+    assert bot.position.facing == 'WEST'
+    assert (0, 1, 'WEST') == bot.report()
+    bot.action_on_robot('RIGHT')
+    assert bot.position.x == 0
+    assert bot.position.y == 1
+    assert bot.position.facing == 'NORTH'
+    assert (0, 1, 'NORTH') == bot.report()
 
 
-def test_robot_movement():
-    pass
+@pytest.mark.parametrize(
+    'place, action, position',
+    [
+        ((0, 0, 'NORTH'), ['MOVE', 'MOVE', 'LEFT'], (0, 2, 'WEST')),
+        ((0, 1, 'NORTH'), ['MOVE'], (0, 2, 'NORTH')),
+        ((0, 2, 'NORTH'), ['LEFT'], (0, 2, 'WEST')),
+        ((0, 3, 'NORTH'), ['LEFT', 'MOVE', 'MOVE', 'RIGHT'], (0, 3, 'NORTH')),
+        ((5, 5, 'NORTH'), ['MOVE', 'MOVE', 'MOVE', 'RIGHT'], (5, 5, 'EAST')),
+    ]
+)
+def test_robot_movement(place, action, position):
+    bot = ToyRobot()
+    bot.place(place)
+    for act in action:
+        bot.action_on_robot(act)
+
+    assert (
+        bot.position.x, bot.position.y, bot.position.facing
+    ) == position
+    assert (
+        bot.position.x, bot.position.y, bot.position.facing
+    ) == bot.report()
